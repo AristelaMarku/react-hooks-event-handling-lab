b02e52b739bf7b8ba3aa70b1dcfb45ab5de4dd22
Aristela Marku <aristela.marku@gmail.com>
2021-10-13 09:23:13 +0200

    commit tree 72ceb1e20ead278187a0b6f6f796f397f1b0259d

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+ function EyesOnMe(){
+
+     function OnFocusEventHandler(){
+       console.log('Good!')
+     }
+
+     function onBloorEventHandler(){
+         console.log('Hey! Eyes on me!')
+     }
+
+    return (
+       
+         <button onFocus={OnFocusEventHandler} onBlur={onBloorEventHandler}>'Eyes on me'</button>
+        
+    )
+ }
+
+ export default EyesOnMe
